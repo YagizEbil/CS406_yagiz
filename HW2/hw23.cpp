f23d82dea78f390b238177b173bc9ca929994af0
yagiz.ebil <yagiz.ebil@sabanciuniv.edu>
2024-11-17 17:55:59 +0000

    Add: Implement SIMD parallelization for matrix multiplication in hw23.cpp

--- HW2/hw23.cpp
+++ HW2/hw23.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <cstdlib>
+#include <immintrin.h> // For SIMD
+#include <omp.h>
 using namespace std;
 
 const int N = 2000;
 
 int main(int argc, char** argv) {
-    float* A = new float[N * N];
-    float* B = new float[N * N];
+    float* A;
+    float* B;
+    posix_memalign((void**)&A, 64, N * N * sizeof(float));
+    posix_memalign((void**)&B, 64, N * N * sizeof(float));
 
     //This part is not modifiable 
     for(int i = 0; i < N; i++) {
@@ -15,14 +20,19 @@ int main(int argc, char** argv) {
         } 
     }
 
-    //TODO: parallelize B = A * A  -----------------------------------------------------------------------
-    //  Copy paste your code to the report and explain the details for your parallelization 
-    //  Report timings with 1,2,4,8 threads  
+    // Parallelize B = A * A with SIMD
+    #pragma omp parallel for collapse(2) schedule(guided)
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < N; j++) {
-            for(int k = 0; k < N; k++) {
-                B[i * N + j] += A[i * N + k] * A[k * N + j];
+            __m256 sum = _mm256_setzero_ps();
+            for(int k = 0; k < N; k += 8) {
+                __m256 a = _mm256_load_ps(&A[i * N + k]);
+                __m256 b = _mm256_load_ps(&A[j * N + k]);
+                sum = _mm256_fmadd_ps(a, b, sum);
             }
+            float temp[8];
+            _mm256_store_ps(temp, sum);
+            B[i * N + j] = temp[0] + temp[1] + temp[2] + temp[3] + temp[4] + temp[5] + temp[6] + temp[7];
         }
     }
     
@@ -35,6 +45,9 @@ int main(int argc, char** argv) {
         }
     }
     cout << sum1 << " " << sum2 << endl;
+
+    free(A);
+    free(B);
 
     return 0;
 }
